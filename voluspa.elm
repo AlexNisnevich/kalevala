1ba5e4ba647dda0601c8157cdaf8e59a83839089
Alex Nisnevich <alex.nisnevich@gmail.com>
2014-11-15 18:15:14 -0800

    basic grid render

--- voluspa.elm
+++ voluspa.elm
@@ -2,6 +2,7 @@
 import Dict
 import Dict (Dict)
 import Mouse
+import Graphics.Collage (Form)
 
 type Board = Dict Location Piece
 
@@ -39,10 +40,9 @@ makeMove move state =
       newBoard = Dict.insert move.location move.piece state.board
       newScore = (Dict.getOrFail p state.score) + (scoreMove move state)
   in
-    {
-      turn = nextPlayer state.turn,
-      board = newBoard,
-      score = Dict.insert p newScore state.score 
+    { turn = nextPlayer state.turn
+    , board = newBoard
+    , score = Dict.insert p newScore state.score 
     }
     
 scoreMove : Move -> State -> Int
@@ -62,15 +62,57 @@ nextPlayer player =
 
 startState : State
 startState = 
-  { 
-    turn = Red,
-    board = Dict.empty, 
-    score = Dict.fromList [("red", 0), ("blue", 0)]
+  { turn = Red
+  , board = Dict.empty
+  , score = Dict.fromList [("red", 0), ("blue", 0)]
   }
   
+drawGrid : Float -> Float -> [Form]
+drawGrid num tileSize =
+  let size = num * tileSize
+      xShift = tileSize / 2 - size / 2
+      yShift = tileSize / 2 - size / 2
+      shape x y = move (tileSize * x + xShift, tileSize * y + yShift) (outlined (solid black) (square tileSize))
+  in
+    (concatMap (\x -> (map (\y -> shape x y) [0..(num - 1)])) [0..(num - 1)])
+
+drawPiece : (Location, Piece) -> Float -> Form
+drawPiece (location, piece) tileSize = 
+  let pos =
+        case piece of
+          Odin -> (3 * tileSize, 1 * tileSize)
+          Thor -> (2 * tileSize, 1 * tileSize)
+          Troll -> (1 * tileSize, 1 * tileSize)
+          Dragon -> (0, 1 * tileSize)
+          Fenrir -> (3 * tileSize, 0)
+          Skadi -> (2 * tileSize, 0)
+          Valkyrie -> (1 * tileSize, 0)
+          Loki -> (0, 0)
+      image = croppedImage pos tileSize tileSize "http://i.imgur.com/5yLICgb.png?1"
+  in
+    toForm image
+
+renderBoard : Board -> Element
+renderBoard board =
+  let num = 15
+      tileSize = 50
+      size = (round num * tileSize) + 1
+      grid = drawGrid num tileSize
+      pieces = map (\p -> drawPiece p tileSize) (Dict.toList board)
+  in
+    collage size size (grid ++ pieces)
+
+display : State -> Element
+display state =
+  flow down 
+    [ asText state
+    , renderBoard state.board 
+    ]
+     
+
 main : Signal Element
 main = 
   -- Move made on every click
   let move = { piece = Odin, location = (0, 0) }
   in
-    asText <~ (foldp tryMove startState ((\() -> move) <~ Mouse.clicks))
+    display <~ (foldp tryMove startState ((\() -> move) <~ Mouse.clicks))
