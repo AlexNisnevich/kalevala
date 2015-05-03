b31233f9c883645ad7e2c251c04a0bc03a95d758
Alex Nisnevich <alex.nisnevich@gmail.com>
2015-05-03 13:49:57 -0700

    refactor

--- Game.elm
+++ Game.elm
@@ -1,34 +1,17 @@
 module Game where
 
-import Array
 import Dict
 import Dict (Dict)
-import Graphics.Element (Element)
-import Graphics.Input.Field (Content)
 import List
 import List (..)
 import Maybe (Maybe (..), withDefault)
-import Mouse
-import Random (Seed, initialSeed)
-import Signal
-import Signal (..)
-import Time
-import Window
-import WebSocket
-import Json.Decode
-import Json.Encode
 
 import Helpers (..)
 import GameTypes (..)
 import Piece
 import Board
 import Player
-import Display
 import AI
-import Serialize
-import Deserialize
-
-import Debug
 
 {- Pick up a piece if it's the given player's turn, otherwise pick up nothing. 
    Returns the new state. -}
@@ -101,25 +84,6 @@ makeMove move state =
             , lastPlaced <- Just move.location
             , delta <- Dict.insert p ("(+" ++ (toString delta) ++ ")") state.delta
             , log <- ((Player.toColor state.turn), logText) :: state.log }
-
-{- Perform an action on the current state and return the resulting state. -}
-performAction : Action -> State -> State
-performAction action state =
-  let p = Player.color state.turn
-      newState =
-        case action of
-          PickUpPiece player idx -> tryToPickUpPiece player idx state
-          PlacePiece mousePos dims -> tryMove (Display.mouseToBoardPosition mousePos state dims) state
-          StartGame gameType deck player playerName -> startGame gameType deck player playerName
-          GameStarted deck startPlayer localPlayer opponentName -> gameStarted deck startPlayer localPlayer opponentName
-          Pass -> { state | turn <- Player.next state.turn }
-          OpponentDisconnected -> { state | gameState <- Disconnected }
-          NoAction -> state
-          ParseError e -> state
-  in
-    if | isGameOver newState -> { newState | gameState <- GameOver }
-       | mustPass newState -> pass newState
-       | otherwise -> newState
 
 {- Does neither player have any tiles left in the given state? -}
 isGameOver : State -> Bool
@@ -232,59 +196,3 @@ startState =
   , delta = Dict.fromList [("red", ""), ("blue", "")]
   , log = []
   }
-
-{- Turn a ClickEvent into an Action -}
-constructAction : ClickEvent -> Seed -> MousePos -> WindowDims -> GameType -> Content -> Action
-constructAction clickType seed mousePos dims gameType playerName =
-  let
-    pos = Debug.watch "Mouse.position" mousePos
-    click = Debug.watch "clickInput.signal" clickType
-  in
-    case clickType of
-      Start -> StartGame gameType (shuffle deckContents seed) (Player.random seed) playerName.string
-      BoardClick -> PlacePiece mousePos dims
-      PieceInHand player idx -> PickUpPiece player idx
-      PassButton -> Pass
-      None -> NoAction
-
-{- Turn a signal of ClickEvents into a signal of Actions -}
-processClick : Signal ClickEvent -> Signal Action
-processClick signal =
-  let seedSignal = (initialSeed << round << fst) <~ Time.timestamp signal
-      sampledMouse = sampleOn signal Mouse.position
-      sampledGameType = sampleOn signal <| subscribe Display.gameTypeChannel
-      sampledPlayerName = sampleOn signal <| subscribe Display.playerNameChannel
-  in
-    constructAction <~ signal ~ seedSignal ~ sampledMouse ~ Window.dimensions ~ sampledGameType ~ sampledPlayerName
-
-{- Path to a Voluspa game server 
-   (see https://github.com/neunenak/voluspa-server) -}
-server : String
-server = "ws://ec2-52-10-22-64.us-west-2.compute.amazonaws.com:22000"
-
-{- The main game loop. 
-   The state is folded over time over a stream of Actions, coming from the client and from the server (for an online game.)
-   Actions from the client are constructed by processing a signal of ClickEvents from Display.clickChannel.
-   In the case the the game type is HumanVsHumanRemote, Actions are also sent to the server over a websocket,
-   and the Actions that are received from the server are merged into the signal of client Actions.
-   For each Action in this resulting signal, (performAction action state) returns the new state. -}
-main : Signal Element
-main =
-  let
-    encode action = Json.Encode.encode 0 (Serialize.action action)
-    decode actionJson = case Json.Decode.decodeString Deserialize.action actionJson of Ok action -> action
-                                                                                       Err e -> ParseError e
-
-    action = processClick (subscribe Display.clickChannel)
-
-    actionWithGameType = (\a t -> (a, t)) <~ action ~ (subscribe Display.gameTypeChannel)
-    actionForRemote = (\(a, t) -> a) <~ keepIf (\(a, t) -> t == HumanVsHumanRemote) (NoAction, HumanVsCpu) actionWithGameType
-
-    request = Debug.watch "request" <~ (encode <~ actionForRemote)
-    response = Debug.watch "response" <~ WebSocket.connect server request
-    responseAction = Debug.watch "deserialized" <~ (decode <~ response)
-
-    state = foldp performAction startState (merge action responseAction)
-
-  in
-    Display.render <~ state ~ Window.dimensions ~ (subscribe Display.gameTypeChannel) ~ (subscribe Display.playerNameChannel)
